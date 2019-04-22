aeadf97644fc3fc1c828895808a9b38ac9fedfb1
Jacob <jkolker@hawk.iit.edu>
2019-04-22 00:25:01 -0500

    clean up JS

--- assets/js/site.js
+++ assets/js/site.js
@@ -36,55 +36,20 @@
     // returns value with all whitespace characters removed
     return value.replace(/\s/g, '');
   }
-
-  // Phone-specific santizier functions
-  function strip_us_country_code(value) {
-    return value.replace(/^1/, '');
-  }
-
-  // All purpose validate function. It takes a value,
-  // along with either a regular expression pattern or
-  // a simple function -- like the comparison functions
-  // above -- and a condition. JavaScript doesn't char
-  // if a function is called with more or fewer arguments
-  // than described in the function definition, so it's
-  // no problem at all to leave off the `condition`
-  // argument when calling a check that's a regular expression
-  function validate(value, check, condition) {
-    if (eq(typeof(check.test), 'function')) {
-      // Handle a regular expression
-      return check.test(value);
-    } else if (eq(typeof(check), 'function')) {
-      // Handle a comparison function
-      return check(value, condition);
-    } else {
-      return false;
-    }
-  }
-
-  // Phone validity functions
-  function validate_us_phone(value) {
-    var phone_number = strip_us_country_code(clean_nonnumbers(value));
-    return validate(phone_number.length, eq, 10);
-  }
-
+  
   // Email validity function
   function validate_email(value) {
     var email = clean_whitespace(value);
     return validate(email, /^[^@\s]+@[^@\s]+$/g);
   }
 
-  // ZIP code validity function
-  function validate_us_zip(value) {
-    var zip = clean_nonnumbers(value);
-    return validate(zip.length, eq, 5);
-  }
 
+//  detect button click to save event info to local storage
   function save_event_info(){
-    //  detect button click to save event info to local storage
+
     document.addEventListener('click', function(event){
       if(event.target.tagName=='A'){
-        //localStorage.clear();
+
         var eventname = event.target.id;
         localStorage.setItem("event-id", eventname);
         localStorage.setItem("event-title", document.getElementById(eventname + "-title").innerHTML);
@@ -130,6 +95,7 @@
 
   }
 
+  // Load event info to signup page
   function set_event_info_signup(){
     if(document.getElementById("form-content")!=null){
     var eventPrice = document.getElementById("event-price");
@@ -138,24 +104,17 @@
     eventName.innerHTML = localStorage.getItem("event-title");
     }
   }
-    
+
+
+  //  Calculate total cost
   function eventPricing(){
     document.addEventListener('click', function(event){
-
 
         var numberOfTickets = document.getElementById("number-tickets-box").value;
         var costOfTickets = document.getElementById("event-price").innerHTML;
         var totalCostItem = document.getElementById("event-total-cost");
 
-
-        /*while(costOfTickets.charAt(0) == '$')
-        {
-         costOfTickets = costOfTickets.substr(1);
-       }*/
         costOfTickets=clean_nonnumbers(costOfTickets);
-
-        //var eventPrice = document.getElementById("event-price");
-        //eventPrice.innerHTML = costOfTickets;
 
         var totalCost = numberOfTickets*costOfTickets;
 
@@ -189,7 +148,7 @@
   }
 
 
-
+  // Validate that string is as long as it you want it to be
   function validate_string_length(string_name, how_long){
     if(string_name.length == how_long){
       return(true);
@@ -204,6 +163,8 @@
     // returns string with no characters that aren't numbers
     return (value.replace(/\D/g,''));
   }
+
+  //  Validate form input from user
   function formValidate() {
     var formItem = document.getElementById("form-content");
 
@@ -306,17 +267,17 @@
   }
   }
 
+  // Generate random order number
   function generateOrderNumber(){
     var orderNum = Math.floor(100000 + Math.random() * 900000);
     return(orderNum);
   }
 
+
+  //  Ave order info after ticket purchase
   function saveOrderInfo(){
 
-
-
     document.addEventListener('click', function(event){
-    //console.log(creditArea);
 
     if(event.target.id == "order"){
 
@@ -344,13 +305,12 @@
       }
 
 
-      console.log("first " + firstName);
+      /*console.log("first " + firstName);
       console.log("last " + lastName);
       console.log("cost " + costPerTicket);
       console.log("num " + numberOfTickets);
       console.log("email " + email);
-      console.log("credit " + creditNum);
-      //console.log(creditArea);
+      console.log("credit " + creditNum);*/
 
       localStorage.setItem("order-first-name", firstName);
       localStorage.setItem("order-last-name", lastName);
@@ -361,6 +321,7 @@
     });
   }
 
+  //  Read order info on to confirmation page
   function readOrderInfo(){
     var pageName = document.getElementById("body-confirmation");
     if(pageName != null){
@@ -386,131 +347,38 @@
   }
 
   document.addEventListener('DOMContentLoaded', function(){
-    // Select the necessary elements from the DOM
-    /*var test = document.getElementById("meeting-your");
-    console.log(test.id + "-add");*/
 
     //  save event info from main page and load to new page
 
     save_event_info();
+
+    //  Load event info on decription page
     set_event_info();
+
+    //  Set event info on signup page
     set_event_info_signup();
+
+    //  calculate price on signup page
     eventPricing();
+
+    // Validate user input on form
     formValidate();
+
+    // On form submission generate order number
     generateOrderNumber();
+
+    //  Ave order info after submission
     saveOrderInfo();
 
+    //  Read order info to confirmation page
     readOrderInfo();
 
+    // Disable form submission button
     var submitButton = document.getElementById("order");
     if(submitButton!=null){
     submitButton.disabled=true;
     }
 
-/*
-    var order = {};
-    var location = {};
-    order.form = document.querySelector('#order-form');
-    order.submit_area = order.form.querySelector('#submit-area');
-    order.submit_button = order.form.querySelector('#order');
-    order.eh_submit_button = document.createElement('a');
-    order.eh_submit_button.href = '#null';
-    order.eh_submit_button.id = 'eh-submit';
-    order.eh_submit_button.setAttribute('role', 'button');
-    order.eh_submit_button.innerText = "Place Enhanced Order";
-
-    // Enhance only for browsers that understand <template>
-    if('content' in document.createElement('template')) {
-      order.size_area = order.form.querySelector('#size-area');
-      order.size_selector = order.form.querySelector('#size');
-      order.eh_size_template = document.querySelector('#size-touch-template');
-      // TODO: Figure out why my event listener failed on this
-      order.eh_size_selector = document.importNode(order.eh_size_template.content, true);
-
-      // Add a hidden class to the old-school select
-      order.size_selector.classList.add('hidden');
-
-      // Replace the select element with a custom, templated control
-      order.size_area.appendChild(order.eh_size_selector);
-
-      // TODO: Make this so I don't cry
-      order.form.querySelector('#size-touch').addEventListener('click', function(e){
-        var size = e.target.dataset.size;
-        var sizes = order.size_selector.querySelectorAll('option');
-        var buttons = order.size_area.querySelectorAll('a');
-        console.log(e.target.dataset.size);
-        // TODO: Clean up how we remove existing selected attribute
-        for (var i=0; i<sizes.length; i++) {
-          sizes[i].removeAttribute('selected');
-          buttons[i].classList.remove('selected');
-        }
-        order.size_selector.querySelector('option[value="'+size+'"]').setAttribute('selected', 'selected');
-        e.target.classList.add('selected');
-
-      });
-
-    }
-
-    // Replace the submit button with `<a role="button">`
-    order.submit_button.classList.add('hidden');
-    order.submit_area.appendChild(order.eh_submit_button);
-
-    location.zip = order.form.querySelector('#zip');
-    location.city = order.form.querySelector('#city');
-    location.state = order.form.querySelector('#state');
-
-    if ('fetch' in window) {
-      var zip;
-      console.log("yay, this browser suppports the Fetch API");
-
-      // TODO: Get rid of this hacky variable to track requests
-
-      location.zip.addEventListener('keyup', function(e){
-        // Validate and ensure no duplicate requests
-        if(validate_us_zip(location.zip.value) && zip !== location.zip.value) {
-          // fetch('http://localhost:8080/60616.js')
-          zip = location.zip.value;
-          fetch('http://api.zippopotam.us/us/' + location.zip.value)
-            .then(function(response){
-              if (response.ok) {
-                return response.json();
-              }
-              throw Error('No data for ZIP code ' + location.zip.value);
-            })
-            .then(function(parsed_json) {
-              location.city.value = parsed_json.places[0]["place name"];
-              location.state.value = parsed_json.places[0]["state"];
-            })
-            .catch(function(error) {
-              console.log(error);
-              location.city.value = '';
-              location.state.value = '';
-            });
-        }
-      });
-
-    }
-
-    // Listen for click events on new submit button, and submit
-    // the form when it's clicked
-    order.eh_submit_button.addEventListener('click', function(event) {
-      // Submit the form
-      event.preventDefault();
-      order.submit_button.click();
-    });
-
-    // Replace the select element with a collection of size buttons
-
-    // Listen for clicks on the size buttons, and set the corresponding
-    // element from the hidden select element
-
-    // Listen for the form's submit event, intercept it and
-    // display an order confirmation where the form once was
-    order.form.addEventListener('submit', function(e){
-      e.preventDefault();
-      console.log('Caught the submit event on JS refactor');
-    });
-*/
   // End of DOMContentLoaded
   });
 
